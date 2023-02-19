c07db634a5f9804990575299cafa0b0065add1fd
Samminenhle <samminenhle@gmail.com>
2023-02-19 21:02:22 +0000

    print

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,7 @@
 def remove_char_at(str, n):
     if n >= 0:
         return str[:n] + str[n+1:]
-     elif abs(n) <= len(str):
-          return str[:n] + str[n+1:]
-       else:
-           raise ValueError("Index out of range")
+    elif abs(n) <= len(str):
+        return str[:n] + str[n+1:]
+    else:
+        raise ValueError("Index out of range")

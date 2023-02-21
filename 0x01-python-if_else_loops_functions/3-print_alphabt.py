c44477da937bc30530c2ecd4f7415b73c56adbf6
Samminenhle <samminenhle@gmail.com>
2023-02-21 23:49:38 +0000

    print

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 123):
     if chr(i) == "q" or chr(i) == "e":
-         pass
-     else:
-          print("{}".format(chr(i)), end="")
+        pass
+    else:
+        print("{}".format(chr(i)), end="")

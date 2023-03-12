c54e316020a0da2b8c0a518e84d4b0a8c6dfffed
Samminenhle <samminenhle@gmail.com>
2023-03-12 21:28:10 +0000

    list

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     all_dir = dir(hidden_4)
-    __ = "__"
-    for i in range(len(all_dir)):
-        if __ not in all_dir[i]:
-            print(all_dir[i])
+    for name in all_dir:
+        if not name.startswith('_'):
+            print(name)

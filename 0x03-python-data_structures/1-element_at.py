4692e5ae4f5236203dc524433df1722fb23ac555
Samminenhle <samminenhle@gmail.com>
2023-03-12 20:48:09 +0000

    list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,10 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
+
     len_of_my_list = len(my_list) - 1
+
     if idx > len_of_my_list:
         return None
-     return my_list[idx]
+
+    return my_list[idx]

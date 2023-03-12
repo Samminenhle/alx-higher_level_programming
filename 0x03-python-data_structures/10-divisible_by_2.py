01e4080bff60d801b8c7aa13ad36fac838a2d108
Samminenhle <samminenhle@gmail.com>
2023-03-12 19:58:50 +0000

    division

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
+def divisible_by_2(my_list=None):
     if my_list is None:
         return
     new_list = list()
     for i in my_list:
         new_list.append(True) if i % 2 == 0 else new_list.append(False)
+
         return new_list
